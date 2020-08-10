04ce2e4dfa064d9617a4c4e10c26a582b4831ce3
battalucar <battalucar@outlook.com>
2020-08-10 21:54:38 +0300

    first commit

---
+++ ToDoApp - Flask/todo.py
@@ -0,0 +1,46 @@
+from flask import Flask,render_template,url_for,redirect,request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Casper/OneDrive/Masaüstü/ToDoApp/todo.db'
+db = SQLAlchemy(app)
+
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    complete = db.Column(db.Boolean)
+
+@app.route("/")
+def index():
+    todos = ToDo.query.all()
+    return render_template("index.html",todos = todos   )
+
+@app.route("/add", methods=["POST"])
+def addToDo():
+    title = request.form.get("title")
+    newToDo = ToDo(title = title, complete = False)
+    db.session.add(newToDo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/complete/<string:id>")
+def complete_todo(id):
+    todo = ToDo.query.filter_by(id=id).first()
+    """if todo.complete == True:
+        todo.complete = False
+    else:
+        todo.complete = True"""
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route("/delete/<string:id>")
+def deleteToDo(id):
+    todo = ToDo.query.filter_by(id=id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
